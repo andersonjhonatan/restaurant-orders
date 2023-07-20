51bd9d1d1c5c30faef4f1ee7295b09cd7a2a5569
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-07-20 17:33:43 -0300

    Feat: Two Commit

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,36 @@
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 2
 def test_dish():
-    pass
+    camarao = Dish("camarão", 25.49)
+    peixada = Dish("peixada", 85.00)
+
+    with pytest.raises(ValueError):
+        Dish("camarão", -77.55)
+
+    with pytest.raises(TypeError):
+        Dish("camarão", "77.55")
+
+    assert camarao.name == "camarão"
+    assert camarao.recipe == {}
+    assert camarao.price == 25.49
+
+    assert camarao.get_ingredients() == set()
+    assert camarao == Dish("camarão", 25.49)
+    assert camarao != Dish("peixada", 75.50)
+
+    assert hash(camarao) == hash(camarao)
+    assert hash(camarao) != hash(peixada)
+    assert repr(camarao) == "Dish('camarão', R$25.49)"
+    assert repr(camarao) != "Dish('peixada', R$75.50)"
+
+    camarao.add_ingredient_dependency(Ingredient("salmão"), 2)
+    assert camarao.get_restrictions() == {
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT,
+    }
+    assert camarao.get_ingredients() == {Ingredient("salmão")}
