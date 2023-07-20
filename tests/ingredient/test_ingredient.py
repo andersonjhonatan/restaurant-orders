63c1d6122830d13daf91a65a7912d7d2a6a77e6c
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-07-20 16:50:51 -0300

    Feat: One Commit

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    resultLasanha= Ingredient('massa de lasanha')
+
+    assert resultLasanha.name == 'massa de lasanha'
+    assert resultLasanha == Ingredient('massa de lasanha')
+
+    assert hash(resultLasanha) == hash('massa de lasanha')
+
+    assert Restriction.GLUTEN in resultLasanha.restrictions
+    assert Restriction.LACTOSE in resultLasanha.restrictions
+
+    assert repr(resultLasanha) == "Ingredient('massa de lasanha')"
